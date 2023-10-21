97c6c3c114ed5dfd7bf783fc9a203937b747810a
lhemakumar <hemakumar.lagudu@gmail.com>
2023-10-21 09:50:13 +0100

    update ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region =var.region
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
 
   tags = {
