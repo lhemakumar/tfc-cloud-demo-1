3607f902fb27b1c8b232d79bb2c64bb2bc66d4d5
lhemakumar <hemakumar.lagudu@gmail.com>
2023-10-21 09:01:28 +0100

    uptick aws terraform version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         aws={
             source="hashicorp/aws"
-            version="~> 3.28.0"
+            version="~> 4.16"
         }
 
         random={
@@ -11,5 +11,5 @@ terraform {
         }
     }
 
-    required_version = ">= 0.14.0"
+    required_version = ">= 1.2.0"
 }
